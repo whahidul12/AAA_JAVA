a0d30701e7238a132e5770670055dea332d7ac20
whahidul12 <mdislamwhahid@gmail.com>
2025-05-21 01:48:33 +0600

    add the changes

---
+++ Practice_Java_001/Practie Java/src/Main.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num1 = sc.nextInt();
+        System.out.print("Enter a number: ");
+        int num2 = sc.nextInt();
+        System.out.print("Enter a number: ");
+        int num3 = sc.nextInt();
+        if (num1 > num2 && num1 > num3) {
+            System.out.println("num1");
+        } else if (num < 0) {
+            System.out.println("n");
+        } else {
+            System.out.println("z");
+        }
+        sc.close();
+    }
+}
