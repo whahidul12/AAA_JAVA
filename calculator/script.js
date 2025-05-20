1d4706812b5554ec5db7da30a40c5a5012e9a345
whahidul12 <mdislamwhahid@gmail.com>
2025-05-21 00:32:37 +0600

    add this three files

--- calculator/script.js
+++ calculator/script.js
@@ -1,14 +1,12 @@
 
-// const one = () => {
-//   let canvas = "";
-//   num = document.querySelector("#button").value;
-//   canvas += num;
-//   document.querySelector(".input").innerHTML = canvas
-//   console.log("one ", num, document.querySelector("#button"));
-// }
 const input = document.querySelector(".input");
 const inputValue = (value) => {
 
   input.textContent += value
+  console.log(input.textContent);
+}
+const submit = (value) => {
+
+  input.textContent = eval(input)
   console.log(input.textContent);
 }
